2effcbd444bb286571b3bac64c61377c9e39615d
Amaury Bourel <amaurybourel@laposte.net>
2023-01-29 17:10:59 +0100

    cssbgdelamortquitue

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -78,7 +78,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function getUserIdentifier(): string
     {
-        return (string) $this->email;
+        return (string) $this->firstName; //email;
     }
 
     /**
